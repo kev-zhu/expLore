32b7b79800faf796cd30ff3975206b2c403178fb
Kevin Zhu <kevinzhu35@gmail.com>
2023-06-08 21:16:18 -0700

    rearraged load-map.js + fixed aysnc geolocation problem

--- gamify/gamify/static/js/load-map.js
+++ gamify/gamify/static/js/load-map.js
@@ -2,23 +2,52 @@
 mapboxgl.accessToken = 'pk.eyJ1Ijoia2V2aW56aHUzNSIsImEiOiJjbGlqZDlucXYwNjZuM3Fxdmd6eTNhMDlrIn0.ULZWndcTJElOGpeFuBAXEw';
 
 const markerStartDiameter = 40
-let currentLoc = null
-let map = null
 
-const getGeoLoc = async () => {
-    try {
-        const pos = await new Promise((resolve, reject) => {
-            navigator.geolocation.getCurrentPosition(resolve, reject)
-        })
-        currentLoc = [pos.coords.longitude, pos.coords.latitude]
-    } catch {
-        console.log('geolocation off')
+let currentLoc = null
+let spinEnabled = true
+let map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    zoom: 2,
+})
+
+map.on('zoom', () => {
+    const markers = document.querySelectorAll('.marker')
+    let adjustedIconZoom = 1 + (map.getZoom() - 13) / 2.5
+
+    if (adjustedIconZoom < 1) {
+        adjustedIconZoom = 1
     }
 
-}
+    markers.forEach(marker => {
+        marker.style.width = `${markerStartDiameter * adjustedIconZoom}px`
+        marker.style.height = `${markerStartDiameter * adjustedIconZoom}px`
+    })
+})
+
+//search
+const geocoder = map.addControl(
+    new MapboxGeocoder({
+        accessToken: mapboxgl.accessToken,
+        mapboxgl: mapboxgl
+    })
+    .on('result', (res) => {
+        //enable spin only important for first instance if no geolocation found -- else this does nothing really
+        spinEnabled = false
+        reverseGeoSearch(res.result.geometry.coordinates)
+
+        //function to clear all pins
+        //function to add all userly saved pins -- prob have to use a fetch to server api to get locations of where user saved
+
+        //when checking out new area, remove all old pins from prev area that are not saved to user's saved data
+        //this prevents overcrowding of pins
+    }),
+    'top-left'
+)
+
 
 const pinnedMarker = (ln, lt, area) => {
-    //change type to read off window/selected buttons -- activity + food
+    //change type= to read off window/selected buttons -- activity + food
     fetch(`/get-poi?lng=${ln}&lat=${lt}&area=${area}&type=activity+food`)
         .then(res => res.json())
         .then(data => {
@@ -55,110 +84,96 @@ const reverseGeoSearch = ([lng, lat]) => {
     })
 }
 
-
-
-getGeoLoc()
-
-//getGeoLoc if available before rendering map
-window.addEventListener('load', () => {
-    console.log(currentLoc)
-    if (currentLoc) {
-        map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v12',
+//get curr location if geolocation turned on
+const getGeoLoc = async () => {
+    try {
+        const pos = await new Promise((resolve, reject) => {
+            navigator.geolocation.getCurrentPosition(resolve, reject)
+        })
+        currentLoc = [pos.coords.longitude, pos.coords.latitude]
+        
+        //if there is a geolocation
+        //fly over there
+        map.flyTo({
             center: currentLoc,
-            zoom: 13
+            zoom: 13,
+            essential: true,
         })
-        userInteracting = true
         spinEnabled = false
-    } else {
-        map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v12',
-            zoom: 2
-        })
-        // map.scrollZoom.disable()
-    }
 
-    const geocoder = map.addControl(
-        new MapboxGeocoder({
-            accessToken: mapboxgl.accessToken,
-            mapboxgl: mapboxgl
-        })
-        .on('result', (res) => {
-            reverseGeoSearch(res.result.geometry.coordinates)
-
-            // do something to remove old markers? maybe? or display all the ones that are "Saved" 
-            //      this means create some saved info from the user where you can call back later
-        }),
-        'top-left'
-    )
-    
-    //marker1 is user's current location or where their search is to
-    // if currentLoc is not null -- geoLocation found at beginning
-    if (currentLoc !== null) {
+        //pin current location
         const marker1 = new mapboxgl.Marker({ color: 'red' })
         .setLngLat(currentLoc)
         .addTo(map);
-    
+
+        //call stuff to add pinned locations
         reverseGeoSearch(currentLoc)
+    } catch {
+        console.log('Your geolocation is currently off.')
     }
+}
 
 
-    //map add eventlistener on scroll - get scrollzoom and scale the width/height of icons
-    map.on('zoom', () => {
-        const markers = document.querySelectorAll('.marker')
-        let adjustedIconZoom = 1 + (map.getZoom() - 13) / 2.5
 
-        if (adjustedIconZoom < 1) {
-            adjustedIconZoom = 1
-        }
+//all of this so far is "start map app"
+//but also have a call to geolocation running on the side before "start map app"
+    //once geo location found, then "fly" over to that spot and pin + stop spin
+    //if not found then just keeps spinning
+    //also add event listener -- if any user interaction on maps, stop spin 
+        //add feature -- double left/right click -- pop up wiht option to explore --> reverse geo serach clicked location
 
-        markers.forEach(marker => {
-            marker.style.width = `${markerStartDiameter * adjustedIconZoom}px`
-            marker.style.height = `${markerStartDiameter * adjustedIconZoom}px`
-        })
-    });
-})
-
-
-// const secondsPerRevolution = 120
-// const maxSpinZoom = 5
-// const slowSpinZoom = 3
- 
-// let spinEnabled = true
-
-//spinglobe demanding -- probably bc of rerendering?
-// const spinGlobe = () => {
-//     const distancePerSecond = 360 / secondsPerRevolution
-//     if (spinEnabled && !userInteracting) {
-//         const center = map.getCenter()
-//         center.lng -= distancePerSecond
-//         map.easeTo({ center, duration: 1000, easing: (n) => n })
-//     }
-// }
-
-// map.on('moveend', () => {
-//     if (spinEnabled) {
-//         spinGlobe()
-//     }
-// })
-
-// map.on('load', () => {
-//     console.log('A load event occurred.');
-//     });
+const startMapApp = () => {
+    const secondsPerRevolution = 120
     
-// spinGlobe()
+    const spinGlobe = () => {
+        const distancePerSecond = 360 / secondsPerRevolution
+        if (spinEnabled) {
+            const center = map.getCenter()
+            center.lng -= distancePerSecond
+            map.easeTo({ center, duration: 1000, easing: (n) => n })
+        }
+    }
+    
+    //start spin and continue spin
+    spinGlobe()
+    map.on('moveend', () => {
+        if (spinEnabled) {
+            spinGlobe()
+        }
+    })
+       
+    //event listeners to stop spin on any user interactions
+    const disableSpinEvents = ['wheel', 'mousedown']
+
+    disableSpinEvents.forEach(event => {
+        map.on(event, () => {
+            spinEnabled = false
+        })
+    })
+}
+
+
+//if user does not have set geolocation -- they can still have pinned/saved locations
+    //ensure that pins are tehre + rotation even if no set geolocation
+    //start map --> load saved locations --> getgeoloc
+const run = async () => {
+    startMapApp()
+    await getGeoLoc()
+    //add a feature that lists the saved locations on side menu and feature to "quick hop" to that place
+    //like a list -- and indented are saved restraunt/activites that person can quick hop to specifically
+    //render all other saved locations -- "places wanted to travel"
+}
+
+run()
 
 
 
+// // // https://docs.mapbox.com/mapbox-gl-js/api/map/#map.event:styleimagemissing
+// // // map.on('style.load', () => {
+// // //     document.querySelector('#map').removeAttribute('hidden')
+// // // })
 
-// // https://docs.mapbox.com/mapbox-gl-js/api/map/#map.event:styleimagemissing
-// // map.on('style.load', () => {
-// //     document.querySelector('#map').removeAttribute('hidden')
-// // })
-
-// // map.on('data', (e) => {
-// //     console.log('asd')
-// //     console.log(e.lngLat)
-// // })
+// // // map.on('data', (e) => {
+// // //     console.log('asd')
+// // //     console.log(e.lngLat)
+// // // })
